39bc1bd01d8e7ef628123b5e1237b754943a47d8
David Mallard <correspondence@davidmallard.id.au>
2017-08-01 09:27:02 +1000

    Remove a semicolon for consistency

--- 2-react-fundamentals/contacts/src/App.js
+++ 2-react-fundamentals/contacts/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
