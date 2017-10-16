b11a2b351db204982d2683fa55ccd6eba1bedb84
David Mallard <correspondence@davidmallard.id.au>
2017-10-16 21:50:35 +1100

    Add the initial file for the Popular Ice Cream quiz

---
+++ 3-react-redux/01-why-redux/06-arrays-reduce-method/03-popular-ice-cream.js
@@ -0,0 +1,20 @@
+/* Popular Ice Cream Totals Quiz
+ *
+ * Using the data array and .reduce():
+ *   - Return an object where each property is the name of an ice cream flavor
+ *     and each value is an integer that's the total count of that flavor
+ *   - Store the returned data in a new iceCreamTotals variable
+ *
+ * Notes:
+ *   - Do not delete the data variable
+ *   - Do not alter any of the data content
+ */
+
+const data = [
+    { name: 'Tyler', favoriteIceCreams: ['Strawberry', 'Vanilla', 'Chocolate', 'Cookies & Cream'] },
+    { name: 'Richard', favoriteIceCreams: ['Cookies & Cream', 'Mint Chocolate Chip', 'Chocolate', 'Vanilla'] },
+    { name: 'Amanda', favoriteIceCreams: ['Chocolate', 'Rocky Road', 'Pistachio', 'Banana'] },
+    { name: 'Andrew', favoriteIceCreams: ['Vanilla', 'Chocolate', 'Mint Chocolate Chip'] },
+    { name: 'David', favoriteIceCreams: ['Vanilla', 'French Vanilla', 'Vanilla Bean', 'Strawberry'] },
+    { name: 'Karl', favoriteIceCreams: ['Strawberry', 'Chocolate', 'Mint Chocolate Chip'] }
+];
