8c5a687644b9a6f13dcc732e4a82282e2f372504
David Mallard <correspondence@davidmallard.id.au>
2017-10-17 19:44:34 +1100

    Add the initial file for the Create A Reducer exercise

---
+++ 3-react-redux/02-redux-at-its-core/03-create-reducers.js
@@ -0,0 +1,14 @@
+/* Create A Reducer
+ *
+ * You need to create a reducer called "appReducer" that accepts two arguments:
+ * - First, an array containing information about ice cream
+ * - Second, an object with a 'DELETE_FLAVOR' `type` key
+ * (i.e., the object contains information to delete the flavor from the state)
+ *
+ * The action your reducer will receive will look like this:
+ * { type: 'DELETE_FLAVOR', flavor: 'Vanilla' }
+ *
+ * And the initial state will look something like this (as such, refrain
+ * from passing in default values for any parameters!):
+ * [{ flavor: 'Chocolate', count: 36 }, { flavor: 'Vanilla', count: 210 }];
+*/
