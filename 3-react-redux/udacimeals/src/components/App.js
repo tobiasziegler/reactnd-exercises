c77890278528de065b278d7b557b99aaa68e81d6
David Mallard <correspondence@davidmallard.id.au>
2017-10-18 20:01:31 +1100

    Use connect()

--- 3-react-redux/udacimeals/src/components/App.js
+++ 3-react-redux/udacimeals/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class App extends Component {
     render() {
@@ -10,4 +11,21 @@ class App extends Component {
     }
 }
 
-export default App
+function mapStateToProps(calendar) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+    return {
+        calendar: dayOrder.map((day) => ({
+            day,
+            meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+                meals[meal] = calendar[day][meal]
+                ? calendar[day][meal]
+                : null
+
+                return meals
+            }, {})
+        }))
+    }
+}
+
+export default connect(mapStateToProps)(App)
