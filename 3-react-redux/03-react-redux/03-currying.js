4065eb402b5f5bc0c16e88df1a1354f0511977fb
David Mallard <correspondence@davidmallard.id.au>
2017-10-18 19:42:54 +1100

    Add the initial file for the Currying exercise

---
+++ 3-react-redux/03-react-redux/03-currying.js
@@ -0,0 +1,15 @@
+/* Write a Curried Function
+ *
+ * Create a function called "houseBuilder" that should:
+ *   - Accept the number of stories (floors)
+ *   - Return a function
+ *
+ * The returned function should:
+ *   - Accept the color of the house
+ *   - Return a string in the following format:
+ *     "building a <numOfStories>-story, <color> house"
+ *
+ * Example:
+ * const response = houseBuilder(3)('blue');
+ * console.log(response); // building a 3-story, blue house
+*/
