7ae1badc95b893639e5cb700316699fa23fa0f03
David Mallard <correspondence@davidmallard.id.au>
2017-11-30 09:23:41 +1100

    Complete the AddEntry-SubmitBtn activities

--- 4-react-native/UdaciFitness/components/AddEntry.js
+++ 4-react-native/UdaciFitness/components/AddEntry.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helpers';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -44,6 +52,27 @@ export default class AddEntry extends Component {
     }));
   };
 
+  submit = () => {
+    const key = timeToString();
+    const entry = this.state;
+
+    // Update Redux
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0
+    }));
+
+    // Navigate to home
+
+    // Save to 'DB'
+
+    // Clear local notification
+  };
+
   render() {
     const metaInfo = getMetricMetaInfo();
 
@@ -75,6 +104,7 @@ export default class AddEntry extends Component {
             </View>
           );
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     );
   }
