00ba08f75a25835b6dd09cd550b6d4d32b989248
David Mallard <correspondence@davidmallard.id.au>
2017-11-30 14:44:25 +1100

    Complete the History-MetricCard activities

--- 4-react-native/UdaciFitness/components/History.js
+++ 4-react-native/UdaciFitness/components/History.js
@@ -13,6 +13,7 @@ import { fetchCalendarResults } from '../utils/api';
 import UdaciFitnessCalendar from 'udacifitness-calendar';
 import { white } from '../utils/colors';
 import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
 
 class History extends Component {
   componentDidMount() {
@@ -41,7 +42,7 @@ class History extends Component {
         </View>
       ) : (
         <TouchableOpacity onPress={() => console.log('Pressed!')}>
-          <Text>{JSON.stringify(metrics)}</Text>
+          <MetricCard metrics={metrics} date={formattedDate} />
         </TouchableOpacity>
       )}
     </View>
