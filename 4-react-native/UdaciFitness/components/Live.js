601aa71fd3ae5eb470ea8bc23e81530f4cabae71
David Mallard <correspondence@davidmallard.id.au>
2017-12-01 10:21:00 +1100

    Complete the Live-undetermined activities

--- 4-react-native/UdaciFitness/components/Live.js
+++ 4-react-native/UdaciFitness/components/Live.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors';
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: ''
   };
+
+  askPermission = () => {};
 
   render() {
     const { status, coords, direction } = this.state;
@@ -16,15 +26,23 @@ export default class Live extends Component {
     }
 
     if (status === 'denied') {
-      <View>
-        <Text>Denied</Text>
-      </View>;
+      return (
+        <View>
+          <Text>Denied</Text>
+        </View>
+      );
     }
 
     if (status === 'undetermined') {
-      <View>
-        <Text>Undetermined</Text>
-      </View>;
+      return (
+        <View style={styles.center}>
+          <Foundation name="alert" size={50} />
+          <Text>You need to enable location services for this app.</Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>Enable</Text>
+          </TouchableOpacity>
+        </View>
+      );
     }
 
     return (
@@ -35,3 +53,28 @@ export default class Live extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20
+  }
+});
