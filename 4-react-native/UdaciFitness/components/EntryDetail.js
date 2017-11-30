677f83ee27203ffb9c951cffc5b93f4c33460b03
David Mallard <correspondence@davidmallard.id.au>
2017-12-01 09:27:27 +1100

    Complete the EntryDetail-reset activities

--- 4-react-native/UdaciFitness/components/EntryDetail.js
+++ 4-react-native/UdaciFitness/components/EntryDetail.js
@@ -3,6 +3,10 @@ import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { white } from '../utils/colors';
 import MetricCard from './MetricCard';
+import { addEntry } from '../actions';
+import { removeEntry } from '../utils/api';
+import { timeToString, getDailyReminderValue } from '../utils/helpers';
+import TextButton from './TextButton';
 
 class EntryDetail extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -17,13 +21,27 @@ class EntryDetail extends Component {
     };
   };
 
+  reset = () => {
+    const { remove, goBack, entryId } = this.props;
+
+    remove();
+    goBack();
+    removeEntry();
+  };
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today;
+  }
+
   render() {
     const { metrics } = this.props;
 
     return (
       <View>
         <MetricCard metrics={metrics} />
-        <Text>EntryDetail - {this.props.navigation.state.params.entryId}</Text>
+        <TextButton onPress={this.reset} style={{ margin: 20 }}>
+          RESET
+        </TextButton>
       </View>
     );
   }
@@ -46,4 +64,18 @@ function mapStateToProps(state, { navigation }) {
   };
 }
 
-export default connect(mapStateToProps)(EntryDetail);
+function mapDispatchToProps(dispatch, { navigation }) {
+  const { entryId } = navigation.state.params;
+
+  return {
+    remove: () =>
+      dispatch(
+        addEntry({
+          [entryId]: timeToString === entryId ? getDailyReminderValue() : null
+        })
+      ),
+    goBack: () => navigation.goBack()
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail);
