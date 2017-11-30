8b857522307ec6eaf61c1256a1866ec3a568ec5c
David Mallard <correspondence@davidmallard.id.au>
2017-11-30 11:25:49 +1100

    Complete the connect-AddEntry activities

--- 4-react-native/UdaciFitness/utils/helpers.js
+++ 4-react-native/UdaciFitness/utils/helpers.js
@@ -127,3 +127,9 @@ export function timeToString(time = Date.now()) {
   );
   return todayUTC.toISOString().split('T')[0];
 }
+
+export function getDailyReminderValue() {
+  return {
+    today: "👏 Don't forget to log your data today!"
+  };
+}
